0dfeb6a7ed3bd31c009b6d3d4d06fa3aa2ef8256
notjuli <julidema2002@gmail.com>
2018-10-18 08:51:37 -0300

    classusuario

---
+++ QEQ_09/QEQ_09/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QEQ_09.Models
+{
+    public class Usuario
+    {
+        private int _idUsuario;
+        private string _NomUsuario;
+        private bool _TipoUsuario;
+        private string _Email;
+        private string _Password;
+
+        public int IdUsuario { get => _idUsuario; set => _idUsuario = value; }
+        public string NomUsuario { get => _NomUsuario; set => _NomUsuario = value; }
+        public bool TipoUsuario { get => _TipoUsuario; set => _TipoUsuario = value; }
+        public string Email { get => _Email; set => _Email = value; }
+        public string Password { get => _Password; set => _Password = value; }
+
+
+    }
+}
