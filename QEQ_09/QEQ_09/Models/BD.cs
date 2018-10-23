0677d1b347552fd9d52c593d8f16b01eb3239020
LAUTARO CORDERO DULITZKY <43630850@comunidadort.edu.ar>
2018-10-23 10:06:23 -0300

    lol

--- QEQ_09/QEQ_09/Models/BD.cs
+++ QEQ_09/QEQ_09/Models/BD.cs
@@ -27,21 +27,22 @@ namespace QEQ_09.Models
         public static bool Login (string Email,string pwd)
         {
 
-            bool news = false;
+            bool User = false;
             SqlConnection Conexion = Conectar();
             SqlCommand consulta = Conexion.CreateCommand();
-            consulta.CommandText = "GetNews";
+            consulta.CommandText = "ObtenerUsuario";
             consulta.CommandType = System.Data.CommandType.StoredProcedure;
             consulta.Parameters.AddWithValue("@Email", Email);
             consulta.Parameters.AddWithValue("@Password", pwd);
             SqlDataReader dataReader = consulta.ExecuteReader();
+
             if (dataReader.Read())
             {
-                news = true;
+                User = true;
 
             }
             Desconectar(Conexion);
-            return news;
+            return User;
         }
 
     }
