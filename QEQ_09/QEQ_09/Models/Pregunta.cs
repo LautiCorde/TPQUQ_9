b9b0ddfeed55faefeaafe1bdf59931970b55491a
LAUTARO CORDERO DULITZKY <43630850@comunidadort.edu.ar>
2018-11-01 09:03:11 -0300

    por

---
+++ QEQ_09/QEQ_09/Models/Pregunta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QEQ_09.Models
+{
+    public class Pregunta
+    {
+        int idPregunta;
+        int TipoPregunta;
+        int CategoriaPregunta;
+        int 
+    }
+}
