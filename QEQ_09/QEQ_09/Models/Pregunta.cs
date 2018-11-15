a52ac009e5eae35c814f7a6d1b02ce3c2da1589d
notjuli <julidema2002@gmail.com>
2018-11-15 07:55:04 -0300

    c

--- QEQ_09/QEQ_09/Models/Pregunta.cs
+++ QEQ_09/QEQ_09/Models/Pregunta.cs
@@ -9,17 +9,20 @@ namespace QEQ_09.Models
     {
         int _idPregunta;
         string _Preguntas;
+        int _idCat;
 
-        public Pregunta(int id, string pre)
+        public Pregunta(int id, string pre, int idCat)
         {
             _idPregunta = id;
             _Preguntas = pre;
+            _idCat = idCat;
         }
 
         public Pregunta() { }
 
         public int IdPregunta { get => _idPregunta; set => _idPregunta = value; }
         public string Preguntas { get => _Preguntas; set => _Preguntas = value; }
-   
+        public int idCat { get => _idCat; set => _idCat = value; }
+
     }
 }
