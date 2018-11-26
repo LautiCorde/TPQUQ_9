0eef61e3edd9b73adb996ba4d67344f6509ac8a7
LAUTARO CORDERO DULITZKY <43630850@comunidadort.edu.ar>
2018-11-26 11:54:10 -0300

    Hay que mejorar validacion pero esta mas lindo

--- QEQ_09/Models/Personaje.cs
+++ QEQ_09/Models/Personaje.cs
@@ -7,12 +7,20 @@ using System.ComponentModel.DataAnnotations;
 namespace QEQ_09.Models
 {
 
-    public class Personaje
+  public class Personaje
     {
 
         int idPersonaje;
         string Nombre;
         int fk_Categoria;
+
+        public int IdPersonaje { get => idPersonaje; set => idPersonaje = value; }
+     
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+      
+        public int Fk_Categoria { get => fk_Categoria; set => fk_Categoria = value; }
+   
+
 
         public Personaje(int id, string nom, int fkcat)
         {
@@ -20,14 +28,12 @@ namespace QEQ_09.Models
             Nombre = nom;
             fk_Categoria = fkcat;
         }
-        public Personaje() { }
-
-        [Required(ErrorMessage = "Id inválido.")]
-        public int IdPersonaje { get => idPersonaje; set => idPersonaje = value; }
-        [Required(ErrorMessage = "Nombre inválido.")]
-        public string Nombre1 { get => Nombre; set => Nombre = value; }
-        [Required(ErrorMessage = "Categoria inválida.")]
-        public int Fk_Categoria { get => fk_Categoria; set => fk_Categoria = value; }
+        public Personaje()
+        {
+            IdPersonaje = 0;
+            Nombre = "";
+            fk_Categoria = 0;
+        }
 
     }
 
